4adfd62d4ba4badc1afefd25113547df7263a522
ghostcake13 <mirandaseverns@gmail.com>
2023-02-18 23:55:49 -0800

    axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Search from "./Search";
+import axios from "axios";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
